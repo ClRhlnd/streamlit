a8c4e4b42d38878f0864f15ff0321a079ae955d0
ClRhlnd <60253580+ClRhlnd@users.noreply.github.com>
2023-06-10 22:49:03 +0200

    Update st_write.py

--- st_write.py
+++ st_write.py
@@ -1,6 +1,6 @@
 import numpy as np
 import altair as alt
-import panda as pd
+import pandas as pd
 import streamlit as st
 
 st.header ('st.write')
@@ -10,3 +10,10 @@ df = pd.DataFrame({
     'first column': [1,2,3,4],
     'second column': [10,20,30,40]
     })
+st.write ('Below is a DataFrame:', df, 'Above is a DataFrame')
+df2 = pd.DataFrame(
+    np.random.randn(200, 3),
+    columns=['a','b','c'])
+c=alt.Chart(df2).mark_circle().encode(
+        x='a', y='b', size='c', color='c', tooltip=['a', 'b','c'])
+st.write(c)
